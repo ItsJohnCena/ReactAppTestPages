e2648702eff872b2b11e9d6f75940c43fb997a63
ItsJohnCena <kristy.crazy4pandas.gao@gmail.com>
2017-12-24 20:48:27 -0500

    Testing out window resizing
    Resonse to screen resizing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  //screen resize
+  constructor(props){
+    super(props);
+    this.state={width: '0', height: '0', tester: 'background true'};
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  componentDidMount(){
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions(){
+    var setter;
+    if(this.state.width < 500){
+      setter='nope';
+    }else{
+      setter='background true';
+    } 
+    this.setState({width: window.innerWidth, height: window.innerHeight, tester: setter});
+    console.log(this.state.tester);
+  }
+  //screen resize
+
   render() {
     return (
       <div className="App">
